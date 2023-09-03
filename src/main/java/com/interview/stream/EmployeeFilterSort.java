a794cd7031b4c10f1995deb83ffd87c4621f8a02
praveen-opensort <praveen@opensort.io>
2023-09-04 00:25:46 +0100

    strem questions

--- src/main/java/com/interview/stream/EmployeeFilterSort.java
+++ src/main/java/com/interview/stream/EmployeeFilterSort.java
@@ -1,12 +1,38 @@
 package com.interview.stream;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class EmployeeFilterSort {
 
-    public List<Employee> sortEmployee(List<Employee> input){
-        return input.stream().sorted().collect(Collectors.toCollection(ArrayList::new));
+    public List<Employee> sortEmployee(List<Employee> employees) {
+        return employees
+                .stream()
+                .sorted()
+                .collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    public List<Employee> sortEmployeeBySalary(List<Employee> employees) {
+        return employees
+                .stream()
+                .sorted(Comparator.comparing(Employee::getEmployeeSalary))
+                .collect(Collectors.toList());
+    }
+
+    public Map<String, Integer> countEmployeeByDept(List<Employee> employees) {
+        return employees
+                .stream()
+                .collect(
+                        Collectors.groupingBy(
+                                Employee::getEmployeeDept,
+                                Collectors.collectingAndThen(
+                                        Collectors.counting(),
+                                        Long::intValue
+                                )
+                        )
+                );
     }
 }
--- src/test/java/com/interview/stream/EmployeeFilterSortTest.java
+++ src/test/java/com/interview/stream/EmployeeFilterSortTest.java
@@ -1,12 +1,54 @@
 package com.interview.stream;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Map;
 
-public class EmployeeFilterSortTest {
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
-    public List<Employee> sortEmployee(List<Employee> input){
-        return input.stream().sorted().collect(Collectors.toCollection(ArrayList::new));
+class EmployeeFilterSortTest {
+
+    private EmployeeFilterSort employeeFilterSort;
+    private List<Employee> employees;
+
+    @BeforeEach
+    void setUp() {
+        employeeFilterSort = new EmployeeFilterSort();
+        employees = new ArrayList<>();
+        employees.add(new Employee(1, "Alex", "Engineering", 70000));
+        employees.add(new Employee(2, "John", "HR", 50000));
+        employees.add(new Employee(3, "Zara", "HR", 60000));
+    }
+
+    @Test
+    void sortEmployee() {
+        List<Employee> sortedEmployees = employeeFilterSort.sortEmployee(employees);
+
+        // Validate the sorting logic
+        assertEquals("Zara", sortedEmployees.get(0).getEmployeeName());
+        assertEquals("John", sortedEmployees.get(1).getEmployeeName());
+        assertEquals("Alex", sortedEmployees.get(2).getEmployeeName());
+    }
+
+    @Test
+    void sortEmployeeBySalary() {
+        List<Employee> sortedEmployees = employeeFilterSort.sortEmployeeBySalary(employees);
+
+        // Validate the sorting logic
+        assertEquals("John", sortedEmployees.get(0).getEmployeeName());
+        assertEquals("Zara", sortedEmployees.get(1).getEmployeeName());
+        assertEquals("Alex", sortedEmployees.get(2).getEmployeeName());
+    }
+
+    @Test
+    void countEmployeeByDept() {
+        Map<String, Integer> result = employeeFilterSort.countEmployeeByDept(employees);
+        assertThat(result).isNotNull();
+        assertThat(result.get("Engineering")).isEqualTo(1);
+        assertThat(result.get("HR")).isEqualTo(2);
     }
 }
